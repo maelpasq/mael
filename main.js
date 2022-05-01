727e059d037ccf226f02dab3fac8f2a9c54072b0
Maël Pasqueraud <mael.pasqueraud@gmail.com>
2022-05-01 17:43:20 +0200

    first modif V2

--- main.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function () {
-  if(document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").className = "scroll-active";
-  } else {
-    document.getElementById("navbar").className = "";
-  }
-}
